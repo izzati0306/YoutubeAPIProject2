cf0fa6472b1f5d85674e3a4e9d6ac04ca73c6dd9
izzat <izzat@IZZATII>
2024-10-18 17:05:46 +0800

    Add project files.

---
+++ Models/YoutubeVideoModel.cs
@@ -0,0 +1,7 @@
+﻿public class YouTubeVideoModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ThumbnailUrl { get; set; }
+    public string VideoUrl { get; set; } // Add this property for the video link
+}
